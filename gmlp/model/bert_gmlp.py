f7dd329af3bdcf54783547d3108250cba3a06979
marowalker <binhnguyen2798@gmail.com>
2022-07-27 14:08:09 +0700

    change params

--- gmlp/model/bert_gmlp.py
+++ gmlp/model/bert_gmlp.py
@@ -107,7 +107,7 @@ class BertgMLPModel:
 
         pos_x = tf.keras.layers.Flatten(data_format="channels_first")(pos_x)
         pos_x = tf.keras.layers.LayerNormalization()(pos_x)
-        pos_x = tf.keras.layers.Dropout(constants.DROPOUT)(pos_x)
+        # pos_x = tf.keras.layers.Dropout(constants.DROPOUT)(pos_x)
         pos_x = tf.keras.layers.Dense(6)(pos_x)
 
         synset_x = tf.keras.layers.Flatten(data_format="channels_first")(synset_x)
@@ -117,12 +117,12 @@ class BertgMLPModel:
 
         triple_x = tf.keras.layers.Flatten(data_format="channels_first")(triple_x)
         triple_x = tf.keras.layers.LayerNormalization()(triple_x)
-        triple_x = tf.keras.layers.Dropout(constants.DROPOUT)(triple_x)
+        # triple_x = tf.keras.layers.Dropout(constants.DROPOUT)(triple_x)
         triple_x = tf.keras.layers.Dense(constants.TRIPLE_W2V_DIM)(triple_x)
 
         position_x = tf.keras.layers.Flatten(data_format="channels_first")(position_x)
         position_x = tf.keras.layers.LayerNormalization()(position_x)
-        position_x = tf.keras.layers.Dropout(constants.DROPOUT)(position_x)
+        # position_x = tf.keras.layers.Dropout(constants.DROPOUT)(position_x)
         position_x = tf.keras.layers.Dense(50)(position_x)
 
         x = tf.keras.layers.concatenate([head_x, e1_x, e2_x, pos_x, synset_x, position_x, triple_x])
@@ -142,14 +142,14 @@ class BertgMLPModel:
             inputs=[self.input_ids, self.head_mask, self.e1_mask, self.e2_mask, self.pos_ids, self.synset_ids,
                     self.triple_ids, self.position_1_ids, self.position_2_ids],
             outputs=self._bert_layer())
-        self.optimizer = tf.keras.optimizers.Adam(lr=2e-5)
+        self.optimizer = tf.keras.optimizers.Adam(lr=1e-5)
 
         # self.model.compile(optimizer=self.optimizer, loss='binary_crossentropy', metrics=['accuracy',
         #                                                                                   F1Score(num_classes=2,
         #                                                                                           average="macro",
         #                                                                                           threshold=0.5)])
         self.model.compile(optimizer=self.optimizer,
-                           loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                           loss=tf.keras.losses.BinaryCrossentropy(),
                            metrics=['accuracy', self.f1_score])
         print(self.model.summary())
 
